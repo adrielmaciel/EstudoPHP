8a4d7678cb4bfd873c5fcc0abc8532511021ae3a
Adriel Maciel <adrielmaciel@gmail.com>
2022-05-08 20:40:30 -0300

    primeiras atualizações

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+//https://www.youtube.com/playlist?list=PLwXQLZ3FdTVEau55kNj_zLgpXL4JZUg8I
+
+//usar o padão psr-4 para o Autoloader no php-fig
+require_once 'App/vendor/autoload.php';
+
+$produto = new \App\Model\Produto();
